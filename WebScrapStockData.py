3595db98b46621bf81e8556b7a73dc2266ae1e15
zeltrex7 <93510067+zeltrex7@users.noreply.github.com>
2024-05-21 09:40:25 +0530

    docker file added & webscraping source changed

--- WebScrapStockData.py
+++ WebScrapStockData.py
@@ -1,91 +1,115 @@
 from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
-import datetime
-import time
+from datetime import datetime, timedelta
 import pandas as pd
-from sqlalchemy import create_engine
 import http.client as http
-http.HTTPConnection._http_vsn = 10
+import time
+import random
+http.HTTPConnection._http_vsn = 100
 http.HTTPConnection._http_vsn_str = 'HTTP/1.0'
+request_header = {'User-Agent': 'Mozilla/5.0',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
+        }
 
-engine = create_engine("mysql+pymysql://{user}:{pw}@localhost/{db}"
-                       .format(user="root",
-                               pw="root",
-                               db="stocks"))
 
-current_timestamp = int(datetime.datetime.now().timestamp())
-print(current_timestamp)
-org_name_url = "https://in.investing.com/equities/trending-stocks"
-org_name_list = []
-url = req = Request(org_name_url, headers={'User-Agent': 'Mozilla/5.0'})
-webpage = urlopen(req).read()
-soup = BeautifulSoup(webpage, 'html.parser')
-list_of_Companyname_tags = soup.find_all(["td"], attrs={"class": "col-name"})
-share_org_list = []
-for i in list_of_Companyname_tags:
-    share_org_list.append(i.a['href'])
 
-"""share_org_list = ['state-bank-of-india','reliance-industries','hdfc-bank-ltd','tata-motors-ltd']"""
-for org_name in share_org_list:
-    print(org_name)
-    start = time.time()
+def get_dates():
+    # Get today's date and time
+    today = datetime.now()
+    
+    # Get today's Unix timestamp
+    unix_timestamp_today = int(today.timestamp())
+    
+    # Get the date 20 years ago
+    twenty_years_ago = today - timedelta(days=20*365)
+    
+    # Get the Unix timestamp for the date 20 years ago
+    unix_timestamp_twenty_years_ago = int(twenty_years_ago.timestamp())
+    
+    # Format the dates as strings
+    today_str = today.strftime("%Y-%m-%d %H:%M:%S")
+    twenty_years_ago_str = twenty_years_ago.strftime("%Y-%m-%d %H:%M:%S")
+    
+    return {
+        "today_date": today_str,
+        "today_unix_timestamp": unix_timestamp_today,
+        "twenty_years_ago_date": twenty_years_ago_str,
+        "twenty_years_ago_unix_timestamp": unix_timestamp_twenty_years_ago
+    }
 
-    url = req = Request('https://in.investing.com' + str(org_name) + '-historical-data?end_date=' + str(
-        current_timestamp) + '&st_date=987100200',
-        headers={'User-Agent': 'Mozilla/5.0'})
-    webpage = urlopen(req).read()
+
+
+def get_most_active_stocks_list():
+    BASE_URL_MOST_ACTIVE = "https://finance.yahoo.com/most-active/"
+    res = Request(BASE_URL_MOST_ACTIVE,
+        headers=request_header)
+    webpage = urlopen(res).read()
+
     soup = BeautifulSoup(webpage, 'html.parser')
-    list_of_Date_tags = soup.find_all(
-        ["td", "span"], attrs={"class": "col-rowDate"})
-    list_of_Open_tags = soup.find_all(
-        ["td", "span"], attrs={"class": "col-last_open"})
-    list_of_Close_tags = soup.find_all(
-        ["td", "span"], attrs={"class": "col-last_close"})
-    list_of_High_tags = soup.find_all(
-        ["td", "span"], attrs={"class": "col-last_max"})
-    list_of_Low_tags = soup.find_all(
-        ["td", "span"], attrs={"class": "col-last_min"})
-    list_of_Volume_tags = soup.find_all(
-        ["td", "span"], attrs={"class": "col-volume"})
-    list_of_percentageChng_tags = soup.find_all(
-        ["td", "span"], attrs={"class": "col-change_percent"})
 
-    end = time.time()
-    data = []
-    for date, open, close, high, low, volume, percentageChange in zip(list_of_Date_tags, list_of_Open_tags,
-                                                                      list_of_Close_tags, list_of_High_tags,
-                                                                      list_of_Low_tags, list_of_Volume_tags,
-                                                                      list_of_percentageChng_tags):
-        data.append([date.contents[1].contents[0], open.contents[1].contents[0], close.contents[1].contents[0],
-                     high.contents[1].contents[0],
-                     low.contents[1].contents[0], volume.contents[1].contents[0],
-                     percentageChange.contents[1].contents[0]])
-    print(end," : ",start)
-    print(end - start, " seconds")
-    df = pd.DataFrame(data, columns=[
-                      'date', 'open', 'close', 'high', 'low', 'volume', 'percentageChange'])
-    vol=[]
-    for i in df.volume:
-        i=i.replace(",","")
-        if "K" in i:
-            vol.append(float(i.replace("K",""))*1000)
-        elif "M" in i:
-            vol.append(float(i.replace("M",""))*1000000)
-        elif "B" in i:
-            vol.append(float(i.replace("B",""))*1000000000)
-        else:
-            vol.append(float(i))
-    df['volume'] = vol
-    df['percentageChange'] =df['percentageChange'].str.replace("%","")
-    df['open'] =df['open'].str.replace(",","")
-    df['close'] =df['close'].str.replace(",","")
-    df['high'] =df['high'].str.replace(",","")
-    df['low'] =df['low'].str.replace(",","")
+    org_symbol_list = []
+    data = soup.findAll(["table"])
+    count=0
+    for i in data[0].findAll(['td'],):
+        if count == 10:
+            count=0
+        if(count==0):
+            print('Symbol : ',i.text,end=" | ")
+            org_symbol_list.append(i.text.strip())
+        if(count==1):
+            print("Org Name : ",i.text)
+        count+=1
+    return org_symbol_list
 
-    try:
-        print(df.head())
-        df.to_csv("." + org_name + ".csv", index=None)
-        df.to_sql(org_name, con = engine, if_exists = 'append')
-        print("data saved for org_name : ", org_name)
-    except:
-        continue
+def get_stock_data(org_symbol,start_date,end_date):
+    req = Request('https://finance.yahoo.com/quote/'+org_symbol+'/history?period1='+start_date+'&period2='+end_date,
+        headers=request_header)
+    webpage = urlopen(req).read()
+
+    soup = BeautifulSoup(webpage, 'html.parser')
+    return soup
+
+def get_header(soup):
+    header = []
+    data = soup.findAll(["table"])
+    #print(data)
+    for i in data[0].next_element.findAll(['th']):
+        header.append(i.text[:10].strip())
+    return header,data
+
+
+def dump_data(data,header,org_symbol):
+    with open('./stocks_data/'+org_symbol+'.csv', 'w') as file:
+        for i in header:
+            file.write(f"{i};")
+        file.write('\n')
+        for i in data[0].findAll(['tr']):
+            count=0
+            for j in i.findAll(['td']):
+                file.write(f"{j.text.strip()};")
+                #print(j.text,end=";")  
+                if count == 6:
+                    file.write('\n')
+                    #print("\n")
+                    count=0
+                count+=1
+        print("Records Count :",len(data[0].findAll(['tr'])))
+
+if __name__=="__main__":
+    
+
+    dates = get_dates()
+    print("Today's Date:", dates["today_date"])
+    print("Today's Unix Timestamp:", dates["today_unix_timestamp"])
+    print("Date 20 Years Ago:", dates["twenty_years_ago_date"])
+    print("Unix Timestamp 20 Years Ago:", dates["twenty_years_ago_unix_timestamp"])
+
+    org_symbol_list = get_most_active_stocks_list()
+    
+    for org_symbol in org_symbol_list:
+        print("org : ",org_symbol)
+        soup = get_stock_data(org_symbol=org_symbol,start_date=str(dates["twenty_years_ago_unix_timestamp"]) ,end_date=str(dates["today_unix_timestamp"]))
+        header,data = get_header(soup)
+        dump_data(data=data,header=header,org_symbol=org_symbol)
+        time.sleep(random.randint(1,10))
+    print("Process Complete")
